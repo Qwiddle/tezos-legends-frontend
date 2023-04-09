8c3d20ed11e38da678da39b99e7dc20533a3153a
Andrew Rubenstein <andjruben@gmail.com>
2023-04-09 18:53:44 -0400

    feat: create base card component
    - also provides GridBackground, a css alternative to a svg/vector grid

---
+++ src/app/components/CardPrimary/styles.ts
@@ -0,0 +1,53 @@
+import styled from 'styled-components';
+
+export const CardPrimary = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  padding: 0px;
+  width: 380px;
+  height: 380px;
+  background: ${p => p.theme.backgroundGradient};
+  border: double 4px transparent;
+  border-radius: 1rem;
+  background-origin: border-box;
+  background-clip: padding-box, border-box;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+`;
+
+export const GridBackground = styled.div`
+  width: 100%;
+  height: 100%;
+  background-repeat: repeat;
+  background-color: transparent;
+  background-size: 15px 15px;
+  background-image: linear-gradient(
+      0deg,
+      transparent 24%,
+      ${p => p.theme.backgroundVariant} 25%,
+      ${p => p.theme.backgroundVariant} 26%,
+      transparent 27%,
+      transparent 74%,
+      ${p => p.theme.backgroundVariant} 75%,
+      ${p => p.theme.backgroundVariant} 76%,
+      transparent 77%,
+      transparent
+    ),
+    linear-gradient(
+      90deg,
+      transparent 24%,
+      ${p => p.theme.backgroundVariant} 25%,
+      ${p => p.theme.backgroundVariant} 26%,
+      transparent 27%,
+      transparent 74%,
+      ${p => p.theme.backgroundVariant} 75%,
+      ${p => p.theme.backgroundVariant} 76%,
+      transparent 77%,
+      transparent
+    );
+`;
+
+export const CardImage = styled.div`
+  width: 100%;
+  height: 50%;
+`;
