4c63994dd87a65eed24a802b80d97d3f6b5b31be
Andrew Rubenstein <andjruben@gmail.com>
2023-04-09 19:00:09 -0400

    feat: push page components towards center
    - nfts will clip outside of box, so it's important we put extra padding below the nav bar

--- src/app/pages/HomePage/styles.ts
+++ src/app/pages/HomePage/styles.ts
@@ -4,6 +4,8 @@ import { StyleConstants } from 'styles/StyleConstants';
 export const AppWrapper = styled.div`
   width: 100%;
   height: calc(100vh - ${StyleConstants.NAV_BAR_HEIGHT});
+  padding: 0 1.5rem;
+  padding-top: 12rem;
   display: grid;
   grid-template-columns: repeat(auto-fit, minmax(100px, 1fr));
   grid-template-rows: 1fr auto;
